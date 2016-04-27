741e91aa4cf8e20ffac0a39e8b3ff2ed37b3a496
Javier <javier.cano@netberry.es>
2016-04-27 22:57:31 +0200

    Zebra Plugin

--- src/ios/Zebra.m
+++ src/ios/Zebra.m
@@ -5,12 +5,6 @@
 - (void)print:(CDVInvokedUrlCommand*)command
 {
 
-    NSString* callbackId = [command callbackId];
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Zebra, %@", name];
-
-    CDVPluginResult* result = name;
-    [self success:result callbackId:callbackId];
 }
 
 @end
